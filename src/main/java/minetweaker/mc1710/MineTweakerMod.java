43c9b70b6636b60705538ed09f9db42ac4003545
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-03-13 11:30:50 +0100

    use File.pathSeparator

--- src/main/java/minetweaker/mc1710/MineTweakerMod.java
+++ src/main/java/minetweaker/mc1710/MineTweakerMod.java
@@ -98,7 +98,8 @@ public class MineTweakerMod {
                 new MCFormatter(),
                 new MCVanilla());
 
-        MineTweakerImplementationAPI.logger.addLogger(new FileLogger(new File("logs/minetweaker.log")));
+        MineTweakerImplementationAPI.logger
+                .addLogger(new FileLogger(new File("logs" + File.pathSeparator + "minetweaker.log")));
         MineTweakerImplementationAPI.platform = MCPlatformFunctions.INSTANCE;
 
         File globalDir = new File("scripts");
